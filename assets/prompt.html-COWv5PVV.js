a9c4f947e608b0b438af988d5328c7af420f9734
litongjava <31761981+litongjava@users.noreply.github.com>
2024-04-29 03:08:40 +0000

    Deploying to gh-pages from @ litongjava/litongjava-opencv-utils-docs@e23cba98789cb81102bac7e9cee57fd7cfb95896 🚀

---
+++ assets/prompt.html-COWv5PVV.js
@@ -0,0 +1,10 @@
+import{_ as e,o as a,c as n,e as t}from"./app-DC6voJ4D.js";const i={},s=t(`<h1 id="prompt" tabindex="-1"><a class="header-anchor" href="#prompt"><span>prompt</span></a></h1><h2 id="编写代码文档" tabindex="-1"><a class="header-anchor" href="#编写代码文档"><span>编写代码文档</span></a></h2><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>为代码编写使用文档,包含下面的部分
+
+1. **简介**：描述类或方法的主要功能和应用场景。
+2. **使用示例**：提供具体的代码示例，展示如何使用类或方法。
+3. **方法说明**：
+   - **方法签名**：详细说明方法的访问级别（如 public 或 private）、返回类型、方法名称以及参数。
+   - **参数**：列出每个参数的类型、名称和描述。
+   - **返回值**：描述返回值的类型和意义。
+   - **异常**：如果方法可能抛出异常，说明这些异常的情况。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),l=[s];function r(d,c){return a(),n("div",null,l)}const m=e(i,[["render",r],["__file","prompt.html.vue"]]),p=JSON.parse('{"path":"/prompt.html","title":"prompt","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"编写代码文档","slug":"编写代码文档","link":"#编写代码文档","children":[]}],"git":{"updatedTime":1714360005000,"contributors":[{"name":"litongjava","email":"litongjava@qq.com","commits":1}]},"filePathRelative":"prompt.md"}');export{m as comp,p as data};
